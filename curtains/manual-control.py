5e6ecae1fce2b6bee72403f5999ba0e9562c820a
nkalupahana <avishkank@gmail.com>
2024-08-11 18:08:49 -0700

    Correct rotation

--- curtains/manual-control.py
+++ curtains/manual-control.py
@@ -1,4 +1,4 @@
-from RPi.GPIO import GPIO
+import RPi.GPIO as GPIO
 GPIO.setmode(GPIO.BCM)
 GPIO.setup(2, GPIO.OUT)
 GPIO.setup(3, GPIO.OUT)
@@ -9,14 +9,14 @@ GPIO.output(3, GPIO.HIGH)
 while True:
     key = input("u - up, d - down, q - quit: ")
     if key == 'u':
-        GPIO.output(2, GPIO.LOW)
-        GPIO.output(3, GPIO.HIGH)
-    elif key == 'd':
-        GPIO.output(2, GPIO.HIGH)
         GPIO.output(3, GPIO.LOW)
+        GPIO.output(2, GPIO.HIGH)
+    elif key == 'd':
+        GPIO.output(3, GPIO.HIGH)
+        GPIO.output(2, GPIO.LOW)
     elif key == 'q':
         break
 
-    input("Press Enter to stop")
+    input("Press Enter to stop: ")
     GPIO.output(2, GPIO.HIGH)
     GPIO.output(3, GPIO.HIGH)
